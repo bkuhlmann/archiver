e226b89c1848bab21b19d93a74088d564911bede
Brooke Kuhlmann <brooke@redalchemist.com>
2013-08-07 19:32:26 -0600

    Removed BACKUP_BASE as a user configurable setting.

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ set -e # Exit if any command returns non-zero.
 if [ -e "$HOME/.archiver/settings.sh" ]; then
   source "$HOME/.archiver/settings.sh"
 fi
+export BACKUP_BASE="$BACKUP_ROOT/base"
 export BACKUP_PATH="$BACKUP_ROOT/$BACKUP_NAME"
 export BACKUP_LOG="/tmp/$BACKUP_MACHINE-$BACKUP_NAME-backup.txt"
 
@@ -24,8 +25,8 @@ if [ -z "$1" ]; then
   while true; do
     echo "Usage: run OPTION"
     echo "\nArchiver Options:"
-    echo "  s: Setup machine."
-    echo "  b: Backup machine."
+    echo "  s: Setup current machine."
+    echo "  b: Backup to remote server."
     echo "  q: Quit/Exit."
     echo ''
     read -p "Enter selection: " response
